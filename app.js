06c8d67a766d53c33dae75b3df5d8e1970460594
sara <kenawysarah@gmail.com>
2026-07-20 14:09:59 +0300

    Add central error middleware

--- app.js
+++ app.js
@@ -4,11 +4,14 @@ const app = express();
 const productRouter = require("./routes/productrouts.js");
 const cartRouter = require("./routes/cartroutes.js");
 const authRouter = require("./routes/authRouts.js");
+const errorHandler = require("./middleware/errorMiddleware");
 
 app.use(express.json());
 
 app.use("/api/auth", authRouter);
 app.use("/api/products", productRouter);
 app.use("/api/cart", cartRouter);
+
+app.use(errorHandler);
 
 module.exports = app;
